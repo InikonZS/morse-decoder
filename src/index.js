0e8185495addd7ce8944ac9ddb298f30a9860d58
Inikon <ZhanZS@mail.ru>
2020-03-04 20:53:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,6 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    //console.log(expr);
+    let ex=[];
+    for (i=0;i<expr.length;i+=10){
+        ex.push(expr.substring(i,i+10));
+    }
+    var mtc=[];
+    console.log(MORSE_TABLE[1]);
+    Object.keys(MORSE_TABLE).forEach((it)=>{
+        let mc=""
+        it.split("").forEach((jt)=>{
+            if (jt=="-"){mc+="11"}
+            if (jt=="."){mc+="10"}
+        })
+        while (mc.length<10){mc="0"+mc;}
+        mtc.push(mc);
+    })
+    var res="";
+    ex.forEach((it)=>{
+        let mi=mtc.indexOf(it);
+        if (mi!=-1){
+            res+=Object.values(MORSE_TABLE)[mi];
+        }else{res+=" ";}
+    })
+    console.log(ex);
+    return res;
     // write your solution here
 }
 
